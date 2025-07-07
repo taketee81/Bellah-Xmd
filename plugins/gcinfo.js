e0c3517c4bdada9c1c3515781c11e65ccc58bcee
Tennor-modz <giddysarry@gmail.com>
2025-07-07 20:33:30 +0300

    Add files via upload

---
+++ plugins/gcinfo.js
@@ -0,0 +1,36 @@
+let handler = async (m, { text,Bellah }) => {
+
+ function convertTimestamp(timestamp) {
+  const d = new Date(timestamp * 1000);
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return {
+    date: d.getDate(),
+    month: new Intl.DateTimeFormat('en-US', { month: 'long' }).format(d),
+    year: d.getFullYear(),
+    day: daysOfWeek[d.getUTCDay()],
+    time: `${d.getUTCHours()}:${d.getUTCMinutes()}:${d.getUTCSeconds()}`
+  }
+}
+
+if (!m.isGroup) return m.reply("This command is meant for groups");
+
+let info = await Bellah.groupMetadata(m.chat);
+let ts = await convertTimestamp(info.creation);
+
+try {
+        pp = await Bellah.profilePictureUrl(chat, 'image');
+      } catch {
+        pp = 'https://img1.pixhost.to/images/5480/594774540_skyzopedia.jpg';
+      }
+
+await Bellah.sendMessage(m.chat, { image: { url: pp }, 
+          caption: `_Name_ : *${info.subject}*\n\n_ID_ : *${info.id}*\n\n_Group owner_ : ${'@'+info.owner.split('@')[0]} || 'No Creator'\n\n_Group created_ : *${ts.day}, ${ts.date} ${ts.month} ${ts.year}, ${ts.time}*\n\n_Participants_ : *${info.size}*\n_Members_ : *${info.participants.filter((p) => p.admin == null).length}*\n\n_Admins_ : *${Number(info.participants.length - info.participants.filter((p) => p.admin == null).length)}*\n\n_Who can send message_ : *${info.announce == true ? 'Admins' : 'Everyone'}*\n\n_Who can edit group info_ : *${info.restrict == true ? 'Admins' : 'Everyone'}*\n\n_Who can add participants_ : *${info.memberAddMode == true ? 'Everyone' : 'Admins'}*`
+        }, {quoted: m })
+};
+
+handler.help = ['gcinformation']
+handler.tags = ['groupinfo']
+handler.command = ['gcinfo']
+
+
+module.exports = handler;
