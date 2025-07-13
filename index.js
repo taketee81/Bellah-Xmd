2122fbc043f026739ecc5090438eb38e6ade83de
taketee81 <161117537+taketee81@users.noreply.github.com>
2025-07-13 06:15:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-//base by DGXeon
-//recode by Giddy Tennor 
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-const {
-   spawn
-} = require('child_process')
-const path = require('path')
-
-function start() {
-   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Bot...')
-            p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
-}
-start()
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello from Node.js on Vercel!');
+}).listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
